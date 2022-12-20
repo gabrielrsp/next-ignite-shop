8800ae8a9588812af30875072c8f351bd2118920
gabriel <gabrielrsp18@gmail.com>
2022-12-20 17:00:23 -0300

    configure stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,13 @@
 // esse arquivo atua como o html da pasta public em um projeto react normal
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'; // tratativa para retorno do css a partir do servidor e nao do browser
+
+/*
+ quando o usuario carregar a pagina next, ela vai pelo lado do backend, montar a pagina, ver o codigo css necessário pra pagina
+ e retornar dessa função getCssText retornando pra tag style 
+
+  O servidor consegue ser mais performatico ao carregar o css, do que o browser.
+/ */
 
 export default function Document() {
   return (
@@ -8,6 +16,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
         <Main/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,42 @@
 //everything here is rendered on the root path (index is always homepage)
+
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: "$rocketseat",
+  borderRadius: 4,  //type number, it will be in px by default
+  border: 0,
+  padding: "4px 8px",
+
+  span: {  //nested styled tag
+    fontWeight: "bold"
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 export default function Home() {
   return (
     <>
-      <h1>hello world</h1>
+      <Button>
+        <span>Enviar</span>
+      </Button>
     </>
   )
 }
+
+
+/* 
+A maioria das libs css in js, estilizam o componente em RUN TIME
+Ou seja, aplicam somente no lado frontend
+
+Em uma aplicação normal React, toda a aplicação é feita em RUNTIME, 
+ou seja, se for desabilitado o javascript do browser, a interface some
+
+Diferente da aplicação next que ja retorna de um servidor node, o html ja montado
+para o browser, por isso conseguimos visualizar o html da aplicação mesmo com 
+o JS do browser desabilitado.
+
+*/

2b74c370f8417d44bbeb6310495614ecdbbba488
gabriel <gabrielrsp18@gmail.com>
2023-01-02 22:39:38 -0300

    create product structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,23 @@
+import { useRouter } from "next/router"
+import { ProductContainer, ImageContainer, ProductDetails } from "../../styles/pages/product"
 export default function Product () {
   return (
-    <h1>Product</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,98</span>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, eaque atque quasi nobis doloremque neque eos tempora corrupti sit eligendi explicabo repellendus, reiciendis, tempore ipsa illo excepturi quod accusantium incidunt.</p>
+
+        <button>
+          Comprar agora
+        </button>
+
+      </ProductDetails>
+
+    </ProductContainer>
   )
 }
